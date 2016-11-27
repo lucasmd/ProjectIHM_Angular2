1c29f10ccffb4cf85041af5d0cfb57abbff87b13
lucasmd <lucas.mirande26@gmail.com>
2016-11-27 11:34:20 +0100

    Modifs visuelles, ajout du carousel (js) qui marche pas, il manque le bootstrap.min.js, il faudrait réinstaller un truc propre pour pouvoir mettre des composants

--- ts/Components/m1m-multimedia-manager.ts
+++ ts/Components/m1m-multimedia-manager.ts
@@ -4,18 +4,56 @@ import {CommService, DataInit, MediaServer, MediaRenderer, Media} from "../Servi
 const htmlTemplate = `
 	<section alx-dragdrop id="services" >
         <div class="container">
-            <div id="header" class="row ">
-				<h1>Composant de gestion des ressources multimédias</h1>
-				<h1>{{title}}</h1>
+            <div id="header" class="row">
+				<!--<h1>Composant de gestion des ressources multimédias</h1>
+				<h1>{{title}}</h1>-->
+				<h1 align="center" style="margin-left: 45%"> DA - Player </h1>
+				
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                  <!-- Indicators -->
+                  <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                  </ol>
+                
+                  <!-- Wrapper for slides -->
+                  <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                      <img src="..." alt="...">
+                      <div class="carousel-caption">
+                        ...
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="..." alt="...">
+                      <div class="carousel-caption">
+                        ...
+                      </div>
+                    </div>
+                    ...
+                  </div>
+                
+                  <!-- Controls -->
+                  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+                			
 			</div>
             <div class="row">
-                <div id="serveurs" class="col-md-6">
+                <div id="serveurs" class="col-md-6" >
                     <section>
                         <h3>Liste de vos centres de données</h3>
                         <ul>
                             <li *ngFor="let server of mediaServers" style="list-style: none">
                                 <div class="modal-content" style="color: darkblue">
-                                    <div class="row list-group-item active" style="box-shadow: 1px 1px 12px #555; border-bottom: thin">
+                                    <div class="row list-group-item active" style="box-shadow: 1px 1px 12px #555; border-bottom: thin;">
                                         <h5 align="left">{{server.name}}</h5>&nbsp;&nbsp;
                                         <img align="center" src="{{server.iconURL}}" height="40" width="40"/>
                                     </div>
